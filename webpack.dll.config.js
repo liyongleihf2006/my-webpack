f280462fd9092c4cb84ef08bb59e29360e131a4f
liyongleihf2006 <573044965@qq.com>
2019-01-25 18:45:41 +0800

    升级一下webpack-cli

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.join(__dirname,'dll'),
         filename: '[name].js',
-        libraryTarget:'window',
+        libraryTarget:'umd',
         library:library,
     },
     entry: {
